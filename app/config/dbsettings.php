d4b262cb3aa4dd44a60ed872dc82c9a55b8cac52
koen-benne <57910795+koen-benne@users.noreply.github.com>
2020-03-03 17:48:20 +0100

    Added standard dbsettings.php file
    This file may need to be edited

---
+++ app/config/dbsettings.php
@@ -0,0 +1,6 @@
+<?php
+//Define DB credentials
+define("DB_HOST", "192.168.50.5"); // Change this if you are using a different host
+define("DB_USER", "root"); // The username of the user of the database that will be used
+define("DB_PASS", "root"); // The password of the user of the database that will be used
+define("DB_NAME", "lloydkwartier"); // The name of the database
