7dbabbdb84689bc9a9a69ccedcdd9588d0d78dd4
danny <dannyhu2002@gmail.com>
2020-04-01 15:18:15 +0200

    Notfound page design change

--- app/templates/notfound/index.php
+++ app/templates/notfound/index.php
@@ -3,5 +3,26 @@
 * @var $pageTitle int
 */
 ?>
-<h1><?= $pageTitle; ?></h1>
-<p>Navigeer naar een andere URL! <a href=<?= BASE_PATH; ?>>Ga naar home</a></p>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404 Notfound Page</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<section class="error-page">
+    <div>
+        <h1 data-h1="404">404</h1>
+        <p data-p="PAGE NOT FOUND"><a href=<?= BASE_PATH; ?>>PAGE NOT FOUND</p>
+    </div>
+</section>
+</body>
+
+</html>
